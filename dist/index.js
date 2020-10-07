b71bd0f33fbec8cd5898dd6d7d95a3c6758b5baf
Bryce Blilie <bryce@bluemagma.com>
2020-10-07 16:45:54 -0500

    updated port to match app engine req

--- dist/index.js
+++ dist/index.js
@@ -9,14 +9,14 @@ var body_parser_1 = __importDefault(require("body-parser"));
 var app = express_1.default();
 app.use(cors_1.default());
 app.use(body_parser_1.default.json());
-var port = 4000;
+var port = 8080;
 var lists = [];
-app.post("/save", function (req, res) {
+app.post('/save', function (req, res) {
     console.log(req.body);
     lists = req.body.lists;
     return res.json({ success: true });
 });
-app.get("/load", function (req, res) { return res.json({ lists: lists }); });
+app.get('/load', function (req, res) { return res.json({ lists: lists }); });
 app.listen(port, function () {
     return console.log("Kanban backend running on http://localhost:" + port + "!");
 });
--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,24 @@
-import express from "express"
-import cors from 'cors'
-import bodyParser from 'body-parser'
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
-const app = express()
+const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
 
-const port = 4000
+const port = 8080;
 
-let lists:any[] = [];
+let lists: any[] = [];
 
-app.post("/save", (req, res) => {
-  console.log(req.body)
-  lists = req.body.lists
-  return res.json({ success: true })
-})
+app.post('/save', (req, res) => {
+	console.log(req.body);
+	lists = req.body.lists;
+	return res.json({ success: true });
+});
 
-app.get("/load", (req, res) => res.json({ lists }))
+app.get('/load', (req, res) => res.json({ lists }));
 
 app.listen(port, () =>
-  console.log(`Kanban backend running on http://localhost:${port}!`)
-)
+	console.log(`Kanban backend running on http://localhost:${port}!`)
+);
